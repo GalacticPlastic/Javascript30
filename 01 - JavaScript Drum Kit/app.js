6e94da117e9f434f85230220b07e7aefb2aa69bf
Mary Mederos <emau757@gmail.com>
2016-12-27 13:03:56 -0500

    Moved JS to External File

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,6 +1,21 @@
-$(document).on('ready', function () {
-	playMusic();
+$(document).ready(function() {
+	console.log('Potato!');
+	const keys = document.querySelectorAll('.key');
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+	window.addEventListener('keydown', playSound);
 });
-function playMusic() {
-	console.log("Potato!");
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add('playing');
 }
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+// NOTE:
+// When you have an array, you cannot listen over all of it.
+// You must explicitly loop over each.
